85ce9eaf1e3947d6b895dafa667cd6bf554a10d0
hyww13 <hyww1101@gmail.com>
2017-06-15 02:35:04 +0800

    client: fix padding problem with double width chars

--- src/util.js
+++ src/util.js
@@ -4,10 +4,13 @@ function newline(n) {
 }
 function pad(n, s, f) {
   if(typeof s !== 'undefined') {
+    s = s.toString();
+    let o = s.length;
+    s.replace(/[ -~]/g,a=>{o--;return a});
     if(f)
-      return (new Array(n+1).join(' ') + s).slice(-n);
+      return (new Array(n+1).join(' ') + s).slice(-(n-o));
     else
-      return (s + new Array(n+1).join(' ')).slice(0, n);
+      return (s + new Array(n+1).join(' ')).slice(0, (n-o));
   }
 
   return new Array(n+1).join(' ');
