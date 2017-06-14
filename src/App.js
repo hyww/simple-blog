596d05a702df1816d0ac7a20bae1b932d6df1d7d
hyww13 <hyww1101@gmail.com>
2017-06-14 16:44:21 +0800

    client: post list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+const server = 'http://localhost:5000'
 
-class App extends Component {
+class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: 'Loading...',
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick() {
+    this.context.router.push('/test');
+  }
+  render() {
+    let posts;
+    if(this.state.status !== 'loaded')
+      posts = this.state.status;
+    else {
+      posts = this.state.posts.map((p,i)=>(
+        <Link className="link" to={`/post/${p._id}`} key={p._id}>
+          <span className="index">
+            {i+1}
+          </span>
+          <span className="author">
+            {p.author}
+          </span>
+          <span className="title">
+            {p.title}
+          </span>
+        </Link>
+      ));
+    }
+    return (
+      <div className="list">
+        {posts}
+      </div>
+    )
+  }
+  componentDidMount() {
+    console.log('mounted');
+    fetch(server + '/api/posts').then(res=>{
+      if(!res.ok)
+        throw res.status;
+      return res.json();
+    }).then(json=>{
+      this.setState({ status: 'loaded', posts: json });
+    }).catch(e=>{
+      this.setState({ status: 'Load failed.' });
+      console.log('Error: '+ e);
+    });
+  }
+}
+
+class Blog extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Route exact path="/" component={List}/>
+      </Router>
     );
   }
 }
 
-export default App;
+export default Blog;
